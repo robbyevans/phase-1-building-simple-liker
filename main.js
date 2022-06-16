f5f67c6537577b1c0cc87b335a618f1687601a8a
Evans Rutto <robbyevans001@gmail.com>
2022-06-16 13:13:53 +0300

    commit tree 0c1cf988fa43d48c6e1c1b4f632ed50c32336fd1

--- main.js
+++ main.js
@@ -3,6 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded',()=>{
+  const liker=document.querySelector('.like-glyph')
+  liker .forEach(liker=>{
+    liker.addEventListener('click',()=>{
+      if(liker.innerText===FULL_HEART){
+        liker.classList.remove('activated-heart')
+        liker.innerText=EMPTY_HEART
+      }
+      else{
+        mimicServerCall()
+        .then(()=>{
+          liker.innerText=FULL_HEART
+          liker.classList.add('activated.heart')
+        })
+        .catch(()=>{
+          let error=document.getElementById('modal')
+          error.classList.remove('hidden')
+          let message=document.getElementById('modal-message')
+          message.innerText='Random server error.Try again.'
+          setTimeout(()=>{error.classList.add('hidden')})
+        })
+      
+      }
+    })
+  })
+})
 
 
 
